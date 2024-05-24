06eddf1eb4ba1e435f1956d1ca9abc10eb1bb0bc
termuze0 <wertman99999@gmail.com>
2024-05-24 12:31:47 +0300

    chapter 4

---
+++ chapter4/voting/code/voting1.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint256 yesCount;
+        uint256 noCount;
+    }
+
+    Proposal[] public proposals;
+
+    function newProposal(address _target, bytes calldata _data) external {
+        Proposal memory proposal = Proposal({
+            target: _target,
+            data: _data,
+            yesCount: 0,
+            noCount: 0
+        });
+
+        proposals.push(proposal);
+    }
+}
