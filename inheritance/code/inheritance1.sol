e49d7f709e26586a46d66206868568f7c4497fab
@Nege21 <121441159+negeze@users.noreply.github.com>
2024-05-24 15:05:00 +0300

    Add files via upload

---
+++ inheritance/code/inheritance1.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Hero.sol";
+
+contract Mage is Hero {
+    
+}
+pragma solidity ^0.8.0;
+
+import "./Hero.sol";
+
+contract Warrior is Hero {
+  
+}
---
+++ inheritance/code/inheritance2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Hero.sol";
+
+contract Mage is Hero(50) {
+   
+}
+
+pragma solidity ^0.8.0;
+
+import "./Hero.sol";
+
+contract Warrior is Hero(200) {
+   
+}
